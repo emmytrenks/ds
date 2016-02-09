af49c438c9747597bb3d5d7521206785599bd3b7
gatesyp <ig11@zips.uakron.edu>
2016-02-09 07:11:28 -0500

    cmake files

---
+++ build/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/aqueue.cpp.o"
+  "CMakeFiles/lab1.dir/proj-1.cpp.o"
+  "lab1.pdb"
+  "lab1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
